489208213fe9cf637373dd2884ecda6a9f6f7ed4
Hrithik <slumdog4321@gmail.com>
2024-01-04 11:18:51 +0530

    ex-3 solved guess slots added in UI

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Guess from "../Guess/Guess";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
